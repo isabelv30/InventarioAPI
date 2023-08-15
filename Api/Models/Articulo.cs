47ab78e5572924f9693be5677e883cfe12769a9b
Isabella Jaramillo <ijaramillo@porcicarnes.com>
2023-08-15 12:52:31 -0500

    Cambios Iniciales API proyecto de inventario

---
+++ Api/Models/Articulo.cs
@@ -0,0 +1,9 @@
+﻿namespace Api.Models
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public string Estado { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ Api/Models/Estado.cs
@@ -0,0 +1,9 @@
+﻿namespace Api.Models
+{
+    public class Estado
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
